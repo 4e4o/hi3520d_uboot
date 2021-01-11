fa426eae86129d8b79208510ac7e80c59c721651
computer <computer@debian>
2021-01-11 13:09:51 -0500

    improved make

--- make.sh
+++ make.sh
@@ -7,9 +7,9 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-WORK_DIR="/home/computer/workspace/my_openipc"
+WORK_DIR="/home/computer/workspace/my_openipc/uboot_hi3520d"
 
-OPENIPC_DIR="${WORK_DIR}/chaos_calmer"
+OPENIPC_DIR="${WORK_DIR}/../chaos_calmer"
 LOADER_SRC_DIR="${WORK_DIR}/u-boot-2010.06_openipc"
 REGISTERS_FILE="${OPENIPC_DIR}/package/boot/uboot-hi35xx/files/uboot_tools/reg_info_hi3520d_duo.bin"
 
@@ -24,6 +24,7 @@ elif [ $1 = "w" ] ; then
 	# OpenWrt
 	BUILD_DIR="${WORK_DIR}/wrt_build"
 	STAGINDIR="${OPENIPC_DIR}/staging_dir/toolchain-arm_cortex-a9_gcc-4.8-linaro_uClibc-0.9.33.2_eabi"
+	STAGING_DIR="${STAGINDIR}"
 	TOOLCHAIN="${STAGINDIR}/bin"
 	CROSSRULE=arm-openwrt-linux-uclibcgnueabi-
 elif [ $1 = "b" ] ; then
@@ -39,13 +40,17 @@ fi
 
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
-PATH=$PATH:${TOOLCHAIN} make O="${BUILD_DIR}" -C "${LOADER_SRC_DIR}" ARCH=arm CROSS_COMPILE=${CROSSRULE} distclean
-PATH=$PATH:${TOOLCHAIN} make O="${BUILD_DIR}" -C "${LOADER_SRC_DIR}" ARCH=arm CROSS_COMPILE=${CROSSRULE} hi3520d_config
-PATH=$PATH:${TOOLCHAIN} make O="${BUILD_DIR}" -C "${LOADER_SRC_DIR}" ARCH=arm CROSS_COMPILE=${CROSSRULE} PLATFORM_REFLAGS="-std=gnu90" STAGING_DIR={STAGINDIR} -j$((`nproc`+1))   #>.build_uboot.log 2>&1
+
+make_run() {
+    STAGING_DIR="${STAGINDIR}" PATH=$PATH:${TOOLCHAIN} make O="${BUILD_DIR}" -C "${LOADER_SRC_DIR}" ARCH=arm CROSS_COMPILE=${CROSSRULE} $@
+}
+
+make_run distclean
+make_run hi3520d_config
+make_run PLATFORM_REFLAGS="-std=gnu90" -j$((`nproc`+1))   #>.build_uboot.log 2>&1
 
 dd if=./u-boot.bin of=./fb1 bs=1 count=64
 dd if=${REGISTERS_FILE} of=./fb2 bs=4800 conv=sync
 dd if=./u-boot.bin of=./fb3 bs=1 skip=4864
 cat fb1 fb2 fb3 > ./u-boot_merged.bin
 rm -f fb1 fb2 fb3
-
