79db5d378d1e3f768bf4fc9ed701fa59306b530c
computer <computer@debian>
2021-01-12 05:54:24 -0500

    added fno-toplevel-reorder and objdump.sh changes

--- objdump_make.sh
+++ objdump_make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 shopt -s globstar
-for f in ./sdk_build/**; do
-	f2="${f/sdk_build/wrt_build}"
+for f in ./wrt_build/**; do
+	f2="${f/wrt_build/wrt_build_buggy}"
 	arm-linux-gnueabi-objdump -d "$f" > "${f}.dis"
 	arm-linux-gnueabi-objdump -d "$f2" > "${f2}.dis"
     	echo "$f"
